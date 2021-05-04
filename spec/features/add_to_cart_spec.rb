397736437cf498aace5e08d8e3e2d2d32fe5513c
Adam Hart <adamhayhart@gmail.com>
2021-05-04 14:37:30 -0700

    project complete, needs readme

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.feature "AddToCarts", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They can add to cart" do
+    # ACT
+    visit root_path
+    puts page.html
+    click_on("Add", match: :first)
+
+    # DEBUG
+    
+    # VERIFY
+    expect(page).to have_content ('My Cart (1)')
+    save_screenshot "capy_img_cart.png"
+  end
 end
